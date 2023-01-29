5eafe8205fdd31dcbd7111806d0305f9b6a06235
kwsilence <kamil.95.05@mail.ru>
2023-01-29 04:02:56 +0300

    added image loader

--- app/src/main/java/com/kwsilence/topmoviescompose/di/AppModule.kt
+++ app/src/main/java/com/kwsilence/topmoviescompose/di/AppModule.kt
@@ -1,5 +1,9 @@
 package com.kwsilence.topmoviescompose.di
 
+import android.content.Context
+import coil.ImageLoader
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import com.kwsilence.topmoviescompose.domain.notification.NotificationScheduler
 import com.kwsilence.topmoviescompose.domain.util.InternetConnectionChecker
 import com.kwsilence.topmoviescompose.feature.movie.details.MovieDetailsViewModel
@@ -16,6 +20,24 @@ val appModule = module {
     viewModel { MovieDetailsViewModel(get()) }
     viewModel { ScheduleTimeViewModel(get(), get(), get()) }
     viewModel { ScheduleListViewModel(get(), get()) }
+
     factory<NotificationScheduler> { NotificationSchedulerImpl(get()) }
     factory<InternetConnectionChecker> { InternetConnectionCheckerImpl(get()) }
+
+    single {
+        val context: Context = get()
+        ImageLoader.Builder(context)
+            .memoryCache {
+                MemoryCache.Builder(context)
+                    .maxSizePercent(0.2)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(context.cacheDir.resolve("movie_poster_cache"))
+                    .maxSizePercent(0.02)
+                    .build()
+            }
+            .build()
+    }
 }
--- app/src/main/java/com/kwsilence/topmoviescompose/ui/component/AsyncImageWithProgress.kt
+++ app/src/main/java/com/kwsilence/topmoviescompose/ui/component/AsyncImageWithProgress.kt
@@ -7,16 +7,19 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import coil.ImageLoader
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
 import coil.request.ImageRequest
+import org.koin.androidx.compose.get
 
 @Composable
 fun AsyncImageWithProgress(
     modifier: Modifier = Modifier,
     url: String?,
-    contentDescription: String? = null
+    contentDescription: String? = null,
+    imageLoader: ImageLoader = get()
 ) {
     when (url) {
         null -> Box(modifier = modifier, contentAlignment = Alignment.Center) {
@@ -26,9 +29,9 @@ fun AsyncImageWithProgress(
             modifier = modifier,
             model = ImageRequest.Builder(LocalContext.current)
                 .data(url)
-                .diskCacheKey(url)
-                .memoryCacheKey(url)
+                .crossfade(true)
                 .build(),
+            imageLoader = imageLoader,
             contentDescription = contentDescription
         ) {
             when (painter.state) {
